b9c9bdf9f0f0fb5be52d49d50b22e076868c78c0
Vladislav <andreevvlad443@gmail.com>
2022-12-22 18:17:55 +0300

    MainActivity update + ChooseTopicsActivity implementation initiated

---
+++ app/src/main/java/com/enzulode/vkqualiapp/ChooseTopicsActivity.kt
@@ -0,0 +1,33 @@
+package com.enzulode.vkqualiapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.enzulode.vkqualiapp.ui.components.LaterBar
+
+class ChooseTopicsActivity : ComponentActivity()
+{
+
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+
+		setContent {
+
+			Column(
+				modifier = Modifier.background(Color.Black).fillMaxSize(),
+				horizontalAlignment = Alignment.CenterHorizontally
+			)
+			{
+				LaterBar(text = "Отметьте то, что Вам интересно, чтобы настроить Дзен")
+			}
+
+		}
+	}
+
+}
--- app/src/main/java/com/enzulode/vkqualiapp/MainActivity.kt
+++ app/src/main/java/com/enzulode/vkqualiapp/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.enzulode.vkqualiapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import com.enzulode.vkqualiapp.laterbutton.LaterButton
+import com.enzulode.vkqualiapp.laterbutton.Status
 import com.enzulode.vkqualiapp.ui.theme.VkQualiAppTheme
 
 class MainActivity : ComponentActivity()
@@ -18,8 +26,41 @@ class MainActivity : ComponentActivity()
 	{
 		super.onCreate(savedInstanceState)
 		setContent {
-			VkQualiAppTheme() {
-				
+
+			MainActivityContent()
+
+		}
+	}
+
+	@Composable
+	private fun MainActivityContent()
+	{
+		val laterButtonState = remember {
+			mutableStateOf(Status.Inactive)
+		}
+
+		VkQualiAppTheme()
+		{
+			Row(
+				modifier = Modifier
+					.fillMaxSize()
+					.background(Color.Black)
+				,
+				horizontalArrangement = Arrangement.SpaceAround,
+				verticalAlignment = Alignment.CenterVertically
+			)
+			{
+				LaterButton(
+					title = "Далее",
+					status = laterButtonState.value,
+					modifier = Modifier.pointerInput(Unit)
+					{
+						detectTapGestures {
+							var intent = Intent(this@MainActivity, ChooseTopicsActivity::class.java)
+							startActivity(intent)
+						}
+					}
+				)
 			}
 		}
 	}
