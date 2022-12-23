bc8783b98e22fd0510c072163bba7b7cc6108243
Vladislav <andreevvlad443@gmail.com>
2022-12-24 01:00:14 +0300

    upd: final activity implemented and registered in manifest

---
+++ app/src/main/java/com/enzulode/vkqualiapp/FinalActivity.kt
@@ -0,0 +1,41 @@
+package com.enzulode.vkqualiapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import com.google.relay.compose.BoxScopeInstanceImpl.align
+
+class FinalActivity : ComponentActivity()
+{
+
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+
+		setContent {
+			Box(
+				modifier = Modifier
+					.fillMaxSize()
+					.background(Color.Black)
+					.align(Alignment.Center),
+				contentAlignment = Alignment.Center
+			)
+			{
+				Text(
+					text = "Final page reached",
+					fontSize = 20.sp,
+					color = Color.White
+				)
+			}
+		}
+	}
+
+}
