e1d44b6fcebe5910a34113c60666fd6d6df60774
Vladislav <andreevvlad443@gmail.com>
2022-12-25 03:42:02 +0300

    ui upd: colorscheme updated: defined default colors for application

--- app/src/main/java/com/enzulode/vkqualiapp/ui/theme/Color.kt
+++ app/src/main/java/com/enzulode/vkqualiapp/ui/theme/Color.kt
@@ -8,3 +8,8 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val DefaultTextColor = Color.Black
 val SecondaryTextColor = Color.DarkGray
+
+val DarkBackgroundColor = Color.Black
+val DarkPrimaryColor = Color.White
+val DarkSecondaryColor = Color.DarkGray
+val DarkSurfaceColor = Color.Black
